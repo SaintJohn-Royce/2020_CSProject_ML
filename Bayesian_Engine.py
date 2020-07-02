cf79c1904578f51850f2d4f8117363d2cbded8e1
Saint-John Royce <vzhu6682@uni.sydney.edu.au>
2020-07-02 13:57:39 +1000

    UI additions

--- Bayesian_Engine.py
+++ Bayesian_Engine.py
@@ -52,7 +52,7 @@ class DeepNet(nn.Module):
 # standard procedure: determine the architecture, SGD method, and the loss function
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 regressor = DeepNet(8, 1).to(device)
-optimizer = optim.Adam(regressor.parameters(), lr=0.0005)
+optimizer = optim.Adam(regressor.parameters(), lr=0.001)
 criterion = torch.nn.MSELoss()
 
 # Training and Testing data subdivision and control.
@@ -90,33 +90,60 @@ with torch.no_grad():
 	# Same principles apply here
     test_out = regressor(X_test)
     loss = criterion(test_out, y_test)
+    print("========================================================")
     print("testing loss:   ", loss)
+    print("========================================================")
 
 ### PERFORMANCE REVIEW ###
 # in theory, an accurate model will yield an estimation that is same as the 
 # actual target value in the dataset, thus we test a known point
-X_response_test = [475, 0, 0, 228, 0, 932, 594, 365]
-X_response_test = torch.tensor(X_response_test).float()
+while True:
+	cement 			= input("cement 			(kg/m^3) [100-600]: 	")
+	slag   			= input("slag   			(kg/m^3) [000-400]: 	")
+	ash				= input("ash			(kg/m^3) [000-350]: 	")
+	water 			= input("water			(kg/m^3) [100-300]: 	")
+	sup_plasticizer	= input("super-plasticizer	(kg/m^3) [000-050]: 	")
+	C_Aggre			= input("Coarse-aggregate	(kg/m^3) [500-1300]: 	")
+	F_Aggre			= input("Fine-aggregate		(kg/m^3) [500-1300]: 	")
+	age 			= input("age of sample  		(day(s)) [001-365]: 	")
 
-# By running iterations, we can calculate the mean and standard deviation of
-# the Gaussian distribution of the posterior
-def regression_test(regressor, X_response_test, samples = 100, stds_multiplier = 2):
+	X_response_test = [float(cement), float(slag), float(ash), float(water), 
+					   float(sup_plasticizer), float(C_Aggre), float(F_Aggre), float(age)]
+	X_response_test = torch.tensor(X_response_test).float()
 
-	# create a list of [regressor(X), ... ... regressor(X)]
-	prediction = [regressor(X_response_test) for i in range(samples)]
-	prediction = torch.stack(prediction)
+	# tune parameters
+	samples = 100
+	std_multiplier = 2
 
-	# standard Mean, Standard Deviation, and Confidence Interval calculation
-	means = prediction.mean(axis=0)
-	stds = prediction.std(axis=0)
-	ci_upper = means + (std_multiplier * stds)
-	ci_lower = means - (std_multiplier * stds)
-	print("mean: 		", means)
-	print("STD: 		", stds)
-	print("upper CL: 	", ci_upper)
-	print("lower CL: 	", ci_lower)
-	return
+	# By running iterations, we can calculate the mean and standard deviation of
+	# the Gaussian distribution of the posterior
+	def regression_test(regressor, X_response_test, samples, std_multiplier):
 
-# actually running the regression test
-regression_test(regressor, X_response_test, samples)
+		# create a list of [regressor(X), ... ... regressor(X)]
+		prediction = [regressor(X_response_test) for i in range(samples)]
+		prediction = torch.stack(prediction)
+
+		# standard Mean, Standard Deviation, and Confidence Interval calculation
+		means = prediction.mean(axis=0)
+		stds = prediction.std(axis=0)
+		ci_upper = means + (std_multiplier * stds)
+		ci_lower = means - (std_multiplier * stds)
+		print("mean: 		", means)
+		print("STD: 		", stds)
+		print("upper CL: 	", ci_upper)
+		print("lower CL: 	", ci_lower)
+		return
+
+	# actually running the regression test
+	regression_test(regressor, X_response_test, samples, std_multiplier)
+
+	test_continuity = input("conduct another test? [Y]/[N]: ")
+
+	print("========================================================")
+
+	if test_continuity == "N":
+
+		break
+
+
 
